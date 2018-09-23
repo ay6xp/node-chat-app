b62b26d1147790c4f8ed490abb33387cb9ccc422
Ahmed Youssef <ay6xp@virginia.edu>
2018-09-23 14:42:46 -0400

    adding scripts

--- server/server.js
+++ server/server.js
@@ -2,10 +2,9 @@ const path = require('path');
 const express = require('express');
 const publicPath = path.join(__dirname,'../public');
 
-var port = 3000;
-
+var port = process.env.PORT || 3000;
 var app = express();
-
+//using middleware to server up public folder
 app.use(express.static(publicPath));
 
 app.listen(port, () => {
