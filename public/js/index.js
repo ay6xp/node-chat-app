af4ae640f8955577831a169998951a4306cb9f00
Ahmed Youssef <ay6xp@virginia.edu>
2018-09-29 18:46:44 -0400

    add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -15,9 +15,20 @@ socket.on('disconnect',function() {
 //listening for new messages
 socket.on('newMessage', function(newMessage) {
   var formattedTime = moment(newMessage.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${newMessage.from} ${formattedTime}: ${newMessage.text}`);
-  jQuery('#messages').append(li);
+  var template = jQuery("#message-template").html();
+  var html = Mustache.render(template, {
+    text: newMessage.text,
+    from: newMessage.from,
+    createdAt: formattedTime
+
+  });
+
+  jQuery('#messages').append(html);
+
+  // var formattedTime = moment(newMessage.createdAt).format('h:mm a');
+  // var li = jQuery('<li></li>');
+  // li.text(`${newMessage.from} ${formattedTime}: ${newMessage.text}`);
+  // jQuery('#messages').append(li);
 });
 
 // socket.emit('createMessage', {
@@ -29,12 +40,19 @@ socket.on('newMessage', function(newMessage) {
 
 socket.on('newLocationMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>'); //_blank opens up new tab
-  var a = jQuery('<a target="_blank">My current location</a>');
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href',message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var template = jQuery("#location-message-template").html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    createdAt: formattedTime,
+    url: message.url
+  });
+  jQuery('#messages').append(html);
+  // var li = jQuery('<li></li>'); //_blank opens up new tab
+  // var a = jQuery('<a target="_blank">My current location</a>');
+  // li.text(`${message.from} ${formattedTime}: `);
+  // a.attr('href',message.url);
+  // li.append(a);
+  // jQuery('#messages').append(li);
 
 });
 
