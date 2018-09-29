4fce6ff3a1a3e1e32fbe76f2c6ff695be3772cef
Ahmed Youssef <ay6xp@virginia.edu>
2018-09-29 12:36:46 -0400

    added chat client styles

--- public/js/index.js
+++ public/js/index.js
@@ -39,11 +39,12 @@ socket.on('newLocationMessage', function(message) {
 
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault(); //so page doesnt refesh
+  var messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage', {
     from:'User',
-    text:jQuery('[name=message]').val()
+    text:messageTextbox.val()
   }, function() {
-
+    messageTextbox.val('');
   });
 });
 
@@ -53,13 +54,17 @@ locationButton.on('click', function() {
   if (!navigator.geolocation) {
     return alert('Geolocation not support by your browser.')
   }
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude:position.coords.latitude,
       longitude:position.coords.longitude
     });
   }, function() {
     alert('Unable to fetch location');
+      locationButton.removeAttr('disabled').text('Send Location');
   });
 
 });
--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on('connection', (socket) => {
     console.log("message", message);
     //io.emit emits an event to every single connection
     io.emit('newMessage',generateMessage(message.from,message.text));
-    callback('This is from the server');
+    callback();
     //will sent everyone message but person who sent it
     // socket.broadcast.emit('newMessage', {
     //   from:message.from,
